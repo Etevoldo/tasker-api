f3fa415b67bc34bfc8e39c031b7092092d7f470d
voldo <filipecavoldo@outlook.com>
2024-11-12 15:44:01 -0300

    fixed get route and added update function pipeline

--- routes/todosRoutes.js
+++ routes/todosRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { verifyAuth } = require('../middleware/authVerify.js');
+const { verifyAuth, verifyTaskPerm } = require('../middleware/authVerify.js');
 const controller = require('../controller/todosController.js');
 
 const router = express.Router();
@@ -7,8 +7,8 @@ const router = express.Router();
 router.post('/', verifyAuth, controller.addPost);
 
 router.route('/:id')
-  .get(verifyAuth, controller.addPost)
-  .put(verifyAuth, (req, res) => res.send('put ok'))
+  .get(verifyAuth, (req, res) => res.send('get ok'))
+  .put(verifyAuth, verifyTaskPerm, controller.updatePost)
   .delete((req, res) => res.send('delete ok'));
 
 module.exports = router;
