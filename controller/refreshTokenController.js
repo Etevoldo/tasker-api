813ecbed993ec38577eb06e378371b88e669a7cc
voldo <filipecavoldo@outlook.com>
2024-11-22 16:09:34 -0300

    change refresh token format to jwt and started using mariadb instead

--- controller/refreshTokenController.js
+++ controller/refreshTokenController.js
@@ -1,40 +1,59 @@
 'use strict';
 
-const RedisClient = require('ioredis');
-const uuid = require('uuid');
+const jwt = require('jsonwebtoken');
+const mariadb = require('mariadb');
 
-const client = new RedisClient();
+const connection = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+};
 
-async function createRefreshToken(user) {
+async function createToken(email) {
   try {
-    const key = uuid.v4();
-    await client.hset(
-      key,
-      {
-        'user': user,
-        'expiry': Math.floor(Date.now() / 1000) + (60 * 60) // 1 hour
-      }
-    );
-    return key;
+    const rToken = jwt.sign({
+      email: email,
+      exp: Math.floor(Date.now() / 1000) + (60 * 60) // 1 hour
+    }, process.env.JWT_REFRESH_SECRET);
+
+    // store into db to revoke access in case of detection
+    const conn = await mariadb.createConnection(connection);
+    const query =
+      'INSERT INTO refreshTokens (token, used, user) VALUES (?, ?, ?)';
+    conn.query(query, [rToken, false, email]);
+
+    return rToken;
   } catch(err) {
     console.error(err);
   }
 }
 
-async function getToken(uuid) {
+async function getToken(rToken) {
+  const conn = await mariadb.createConnection(connection);
   try {
-    return await client.hgetall(uuid);
+    const query =
+      'SELECT * FROM refreshTokens WHERE token = ?';
+
+    // get a fresh decode version of the token
+    const tokenTouple = (await conn.query(query, [rToken]))[0];
+
+    return tokenTouple;
   } catch(err) {
     console.error(err);
   }
 }
 
-async function delToken(uuid) {
+async function delToken(rToken) {
+  const conn = await mariadb.createConnection(connection);
   try {
-    return await client.hdel(uuid, ['user', 'expiry']);
+    const query =
+      'DELETE FROM refreshTokens WHERE token = ?';
+
+    await conn.query(query, [rToken]);
   } catch(err) {
     console.error(err);
   }
 }
-
-module.exports = {createRefreshToken, getToken, delToken};
+//TODO implement mark used function
+module.exports = {createToken, getToken, delToken};
